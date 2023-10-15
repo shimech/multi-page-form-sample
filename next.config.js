3046ec571e836d68e0dd5deaa29999b512b442a6
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-10-15 12:36:54 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  assetPrefix: process.env.BASE_PATH ?? "",
+  basePath: process.env.BASE_PATH ?? "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
